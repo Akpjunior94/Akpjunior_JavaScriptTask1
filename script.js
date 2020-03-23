98839c99a577c3650eeefeb8b035595cffc7f04a
unknown <akpjunio4real@gmail.com>
2020-03-23 19:15:19 +0100

    changes made

--- script.js
+++ script.js
@@ -2,30 +2,11 @@ let myName = 'Akpan Abraham';
 var myCourses = ["HTML", "CSS", "JavaScript"];
 
 console.log(myName);
+
 console.log(myCourses);
-console.log(myCourses.length);
-
-
 
 for (i = 1; i <= 200; i++) {
     if (i % 2 !== 0) {
         console.log(i + " is odd");
     }
-
-
-    
-    // if (myCourses.length % 2 === 0) {
-    //     if (i % 2 === 0) {
-    //         console.log(i + " is even");
-    //     }
-    // } else {
-    //     if (i % 2 !== 0) {
-    //         console.log(i + " is odd");
-    //     }
-    // }
 }
-
-
-
-
-
