6ad0e0718a2c9d283499dbe334f500381cee1aff
unknown <akpjunio4real@gmail.com>
2020-03-23 13:10:39 +0100

    new commit

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ let myName = 'Akpan Abraham';
 let myCourses = ["HTML", "CSS", "JavaScript"];
 
 
-for (x=0; x<=200; x++){
-    
+for (x=1; x<=200; x++){
+    if (x === 0) {
+        console.log(x +  " is even");
+    }
+    else if (x % 2 === 0) {
+        console.log(x + " is even");   
+    }
+    else {
+        console.log(x + " is odd");
+    }
 }
